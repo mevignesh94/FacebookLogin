d50c7e069a9dd5db0b3050d27b6f77582f465fb2
mevignesh94 <vickyvig1008@gmail.com>
2021-11-09 11:53:21 +0530

    username functions

--- src/test/java/org/test1/UserName.java
+++ src/test/java/org/test1/UserName.java
@@ -1,5 +1,11 @@
 package org.test1;
 
 public class UserName {
+	public static void userName() {
+		System.out.println("userName");
+	}
+	public static void main(String[] args) {
+		userName();
+	}
 
 }
